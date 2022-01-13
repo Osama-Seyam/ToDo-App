2564a93cddd8ff2c9487f59af4b65e2fd39a79be
Osama-Seyam <osamamostafa065@gmail.com>
2022-01-13 18:37:49 +0200

    Edit !!

--- App/Js/script.js
+++ App/Js/script.js
@@ -6,18 +6,20 @@ let model = document.getElementById('model');
 let show = document.getElementById('show');
 let close = document.getElementById('close');
 
-let showModel = function () {
+let showModel = () => {
     model.classList.toggle('display');
 }
-show.addEventListener('click', showModel);
-close.addEventListener('click', showModel)
 
-let showNoTasks = function () {
+show.addEventListener('click', showModel);
+close.addEventListener('click', showModel);
+
+let showNoTasks = () => {
     if (tasks.childElementCount == 0) {
         noTasks.classList.remove('none');
     }
 }
-let addTask = function () {
+
+let addTask = () => {
     let task = input.value;
     if (task.trim() == 0 || task.length < 2 || task.length > 30) {
         alert("you Must enter Valid Data")
@@ -35,6 +37,7 @@ let addTask = function () {
     })
     input.value = "";
 }
+
 addbtn.addEventListener('click', addTask);
 
 let deleteTask = function (e) {
@@ -46,14 +49,15 @@ let deleteTask = function (e) {
 
 document.addEventListener('click', deleteTask)
 
-// dark-mode
+/* =========
+   dark-mode */
 
 let button = document.getElementById('button');
 let body = document.querySelector('body');
 let taskHeader = document.getElementById('task-Header')
 let modelCard = document.getElementById('modelCard')
 
-button.onclick = function () {
+button.onclick = () => {
     if (body.classList[0] == 'dark') {
         body.classList.remove('dark')
         taskHeader.classList.remove('taskHeader')
